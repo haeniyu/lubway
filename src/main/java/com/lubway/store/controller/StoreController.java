48b3e6dc300472a3182e7556683543e64f230698
jieun <orbital0m0@naver.com>
2021-03-20 18:47:29 +0900

    STORE관리자 매장정보 변경페이지 수정

--- src/main/java/com/lubway/store/controller/StoreController.java
+++ src/main/java/com/lubway/store/controller/StoreController.java
@@ -3,6 +3,8 @@ package com.lubway.store.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,39 +27,37 @@ public class StoreController {
 
 	@Autowired
 	private StoreService storeService;
-	
+
 	@PostMapping("/main.sdo")
-	public String main(@RequestParam("id") String id, 
-			@RequestParam("password") String password, 
-			HttpServletResponse response, 
-			HttpServletRequest request) throws IOException {
+	public String main(@RequestParam("id") String id, @RequestParam("password") String password,
+			HttpServletResponse response, HttpServletRequest request) throws IOException {
 		System.out.println("매장 관리자 메인 화면 이동");
-		
+
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
-		
+
 		StoreVO store = new StoreVO();
 		store.setId(id);
 		StoreVO getStore = storeService.getStore(store);
 		System.out.println(getStore.toString());
-		
-		if(getStore == null) {
+
+		if (getStore == null) {
 			out.println("<script>alert('아이디 또는 비밀번호가 틀렸습니다.'); history.go(-1); </script>");
 			out.flush();
 			out.close();
 			return null;
 		} else {
-				HttpSession session = request.getSession();
-				session.setAttribute("store", getStore);
-				System.out.println("ID, Password 일치");
-				System.out.println("로그인 성공");
-				return "main";
+			HttpSession session = request.getSession();
+			session.setAttribute("store", getStore);
+			System.out.println("ID, Password 일치");
+			System.out.println("로그인 성공");
+			return "main";
 		}
 	}
-	
+
 	@GetMapping("/main.sdo")
 	public String mainView(HttpSession session) {
-		if(session.getAttribute("store") == null) {
+		if (session.getAttribute("store") == null) {
 			return "login";
 		}
 		return "main";
@@ -69,42 +69,49 @@ public class StoreController {
 		session.invalidate();
 		return "login";
 	}
-	
+
 	@RequestMapping("/info.sdo")
 	public String info(HttpSession session) {
 		System.out.println("매장 정보 입력 화면 이동");
-		
+
 		StoreVO vo = (StoreVO) session.getAttribute("store");
 		System.out.println(vo.toString());
-		
+
 		StoreInfoVO getInfo = storeService.getstoreinfo(vo.getStorename());
 		System.out.println(getInfo.toString());
-		
+
 		session.setAttribute("storeinfo", getInfo);
 		return "storeinfo";
 	}
-	
+
 	@RequestMapping("/update.sdo")
-	public String update(@RequestParam("openTime") String openTime, 
-			@RequestParam("closeTime") String closeTime, @RequestParam("morning_usable") boolean morning, 
-			@RequestParam("fastway_usable") boolean fastway, @RequestParam("homeway_usable") boolean homeway,
-			HttpSession session) {
-		
+	public String update(@RequestParam("openTime") String openTime, @RequestParam("closeTime") String closeTime,
+			@RequestParam("morning_usable") boolean morning, @RequestParam("fastway_usable") boolean fastway,
+			@RequestParam("homeway_usable") boolean homeway, HttpSession session) {
+
 		StoreInfoVO store = (StoreInfoVO) session.getAttribute("storeinfo");
+
+		// 이벤트 기간 수정 여부
+		if (!openTime.equals("")) {
+			System.out.println("오픈 시간 수정");
+			Time open = Time.valueOf(openTime + ":00");
+			store.setOpen(open);
+		}
 		
-		Time open = Time.valueOf(openTime+":00");
-		Time close = Time.valueOf(closeTime+":00");
+		if (!closeTime.equals("")) {
+			System.out.println("종료 시간 수정");
+			Time close = Time.valueOf(closeTime + ":00");
+			store.setClose(close);
+		}
 		
-		store.setOpen(open);
-		store.setClose(close);
 		store.setMorning_usable(morning);
 		store.setFastway_usable(fastway);
 		store.setHomeway_usable(homeway);
-		
+
 		System.out.println("매장정보 수정 : " + store.toString());
-		
+
 		storeService.updatestoreinfo(store);
-		
+
 		return "storeinfo";
 	}
 
