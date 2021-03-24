2c97ff9794195b5626cc97f4fba17033931b31af
poong <kxghg12@naver.com>
2021-03-24 15:21:48 +0900

    ㅜㅜ

--- src/main/java/com/lubway/user/controller/SelectMenuController.java
+++ src/main/java/com/lubway/user/controller/SelectMenuController.java
@@ -4,8 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lubway.admin.menu.CookieVO;
+import com.lubway.admin.menu.DrinkVO;
+import com.lubway.admin.menu.MorningVO;
+import com.lubway.admin.menu.NutrientVO;
+import com.lubway.admin.menu.SaladVO;
+import com.lubway.admin.menu.SandwichVO;
+import com.lubway.admin.menu.WedgeAndSoupVO;
+import com.lubway.admin.menu.WrapVO;
+import com.lubway.admin.menu.service.MenuService;
 import com.lubway.user.service.UserMenuService;
 
 @Controller
@@ -14,6 +24,8 @@ public class SelectMenuController {
 	
 	@Autowired
 	private UserMenuService userMenuService;
+	@Autowired
+	private MenuService menuService;
 	
 	/** 메뉴 선택 페이지 */
 	@GetMapping("step02.do")
@@ -30,50 +42,43 @@ public class SelectMenuController {
 		return "fastwaystep02";
 	}
 	
-	/** 메뉴 페이지(샌드위치) */
-	@GetMapping("/menuSandwich.do")
-	public String menuSandwich(Model model) {
-		model.addAttribute("select", "menuSandwich.do");
-		model.addAttribute("list", userMenuService.getSandwichList());
-		model.addAttribute("calList", userMenuService.getSandwichCalList());
-		return "menu/menuSandwich";
+	/** 메뉴 상세 페이지 */
+	@PostMapping("menuDetail.do")
+	public String menuDetail(Model model, CookieVO cvo, SandwichVO Svo, WrapVO wvo, WedgeAndSoupVO wasvo, SaladVO svo,
+			MorningVO mvo, DrinkVO dvo, NutrientVO nvo, String select, String code) {
+		System.out.println(code);
+		System.out.println(select);
+		model.addAttribute("select", select);
+		model.addAttribute("code", code);
+		model.addAttribute("nutrient", menuService.selectNutrient(nvo));
+
+		switch (select) {
+		case "menuSandwich.do":
+			model.addAttribute("menu", menuService.selectSandwich(Svo));
+			System.out.println(menuService.selectSandwich(Svo).getFilePath());
+			break;
+		case "menuWrap.do":
+			model.addAttribute("menu", menuService.selectWrap(wvo));
+			break;
+		case "menuSalad.do":
+			model.addAttribute("menu", menuService.selectSalad(svo));
+			break;
+		case "menuMorning.do":
+			model.addAttribute("menu", menuService.selectMorning(mvo));
+			break;
+		case "menuSmileWay.do":
+			char cod = code.charAt(6);
+			System.out.println(cod);
+			if (cod == 'C') {
+				model.addAttribute("menu", menuService.selectCookie(cvo));
+			} else if (cod == 'W') {
+				model.addAttribute("menu", menuService.selectWAS(wasvo));
+			}
+			break;
+		default:
+			break;
+		}
+		return "menu/menuDetail";
 	}
 
-	/** 메뉴 페이지(랩) */
-	@GetMapping("/menuWrap.do")
-	public String menuWrap(Model model) {
-		model.addAttribute("select", "menuWrap.do");
-		model.addAttribute("list", userMenuService.getWrapList());
-		model.addAttribute("calList", userMenuService.getWrapCalList());
-		return "menu/menuWrap";
-	}
-
-	/** 메뉴 페이지(찹샐러드) */
-	@GetMapping("/menuSalad.do")
-	public String menuSalad(Model model) {
-		model.addAttribute("select", "menuSalad.do");
-		model.addAttribute("list", userMenuService.getSaladList());
-		model.addAttribute("calList", userMenuService.getSaladCalList());
-		return "menu/menuSalad";
-	}
-
-	/** 메뉴 페이지(아침메뉴) */
-	@GetMapping("/menuMorning.do")
-	public String menuMorning(Model model) {
-		model.addAttribute("select", "menuMorning.do");
-		model.addAttribute("list", userMenuService.getMorningList());
-		model.addAttribute("calList", userMenuService.getMorningCalList());
-		return "menu/menuMorning";
-	}
-
-	/** 메뉴 페이지(스마일 웨이) */
-	@GetMapping("/menuSmileWay.do")
-	public String menuSmileWay(Model model) {
-		model.addAttribute("select", "menuSmileWay.do");
-		model.addAttribute("list", userMenuService.getSmileWayCookieList());
-		model.addAttribute("calList", userMenuService.getSmileWayCookieCalList());
-		model.addAttribute("list2", userMenuService.getSmileWayWASList());
-		model.addAttribute("calList2", userMenuService.getSmileWayWASCalList());
-		return "menu/menuSmileWay";
-	}
 }
--- src/main/webapp/WEB-INF/views/user/fastwaystep02.jsp
+++ src/main/webapp/WEB-INF/views/user/fastwaystep02.jsp
@@ -6,7 +6,27 @@
 <html>
 <link rel="stylesheet" type="text/css" href="${path}/resources/css/step01.css" />
 <link rel="stylesheet" type="text/css" href="${path}/resources/css/fastway.css" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".pd_list_wrapper").hide();
+		$("ul.list_menu li:first").addClass("active").show();
+		$(".pd_list_wrapper:first").show();
+		
+		$("ul.list_menu li").click(function(){
+			$("ul.list_menu li").removeClass("active");
+			$("this").addClass("active");
+			$(".pd_list_wrapper").hide();
+			
+			var activeTab = $(this).find("a").attr("href");
+			$(activeTab).fadeIn();
+			return false;
+		});
 
+	});
+
+
+</script>
 <head>
 <meta charset="UTF-8">
 <title>fastway/Step02</title>
@@ -14,6 +34,7 @@
 </head>
 <body>
 	<%@ include file="/WEB-INF/views/user/header.jsp"%>
+	<form action="">
     <div class="content">
             <!-- container s -->
         <div id="container">
@@ -28,47 +49,51 @@
 						</p>
 					</div>
 					<div class="tab02">
-						<ul>
-							<li class="active">
-								<a href="#"  >찹샐러드</a>
-							</li>
-							<li>
-								<a href="#" >샌드위치</a>
-							</li>
-							<li>
-								<a href="#"  >사이드ㆍ음료</a>
-							</li>
-							<li>
-								<a href="#"  >랩ㆍ기타</a>
-							</li>
+						<ul class="list_menu">
+
+							<li><a href="#">샌드위치</a></li>
+							<li><a href="#">랩ㆍ기타</a></li>
+							<li><a href="#">찹샐러드</a></li>
+							<li><a href="#">사이드ㆍ음료</a></li>
 						</ul>
 					</div>
 					<!-- 컨텐츠 리스트 -->
+					
 					<div class="order_con" id="itemListMst">
+						<div class="pd_list_wrapper">
+						
 						<ul>
 							<c:forEach var="list" items="${list}" varStatus="status">
+							
 								<li>
+									<a onclick="javascript:menuDetail('${list.code}');" class="btn_view" href="#" style="display:block;">
 									<div class="img">
 										<img alt="${list.name}" src="${list.filePath}" />
 									</div>
-									
 									<strong class="tit">${list.name}</strong>
 									<span class="eng">${list.engname}</span>
 									<span class="cal">${calList[status.index]} kcal</span>
-									
-									<a onclick="javascript:menuDetail('${list.code}');" class="btn_view" href="#"></a>
+									</a>
 								</li>
 							</c:forEach>
-						</ul>					
+						</ul>
+					</div>	
 					</div>
+					 
 				</div>
 				<!--// index -->
 			</div>
 			<!--// sub content e -->
 			<!-- 메뉴리스트 -->
 		</div>
+
 		</div>
+			<input type="hidden" name="select" value="${select}">
+	</form>	
+		
+		
 <%@ include file="/WEB-INF/views/user/footer.jsp"%>
+
 
 </body>
 </html>
