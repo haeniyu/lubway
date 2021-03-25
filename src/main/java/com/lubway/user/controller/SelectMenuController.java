35ef68758bc82a9b6776e39cf2dfecceda37f597
YunHyuna <hyun_a701@naver.com>
2021-03-25 09:10:32 +0900

    컨트롤러 합침

--- src/main/java/com/lubway/user/controller/SelectMenuController.java
+++ src/main/java/com/lubway/user/controller/SelectMenuController.java
@@ -35,7 +35,17 @@ public class SelectMenuController {
 	
 	/** 메뉴 선택 페이지 */
 	@GetMapping("/step02.do")
-	public String menuSelect() {
+	public String menuSelect(Model model,String code) {
+		model.addAttribute("sandwich", userMenuService.getSandwichList());
+		model.addAttribute("calSand", userMenuService.getSandwichCalList());
+		model.addAttribute("salad", userMenuService.getSaladList());
+		model.addAttribute("calSal", userMenuService.getSaladCalList());
+		model.addAttribute("wrap", userMenuService.getWrapList());
+		model.addAttribute("calWrap", userMenuService.getWrapCalList());
+		model.addAttribute("list", userMenuService.getSmileWayCookieList());
+		model.addAttribute("calList", userMenuService.getSmileWayCookieCalList());
+		model.addAttribute("list2", userMenuService.getSmileWayWASList());
+		model.addAttribute("calList2", userMenuService.getSmileWayWASCalList());
 		return "step02";
 	}
 
