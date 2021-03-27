c9dfec185be15a022040005f99cd2873cb5fea1d
SoulTreeHyun <wldydgus@naver.com>
2021-03-27 13:41:29 +0900

    장바구니

---
+++ src/test/java/com/web/lubway/Test.java
@@ -0,0 +1,40 @@
+//package com.web.lubway;
+//
+//public class Test {
+//	public static void main(String[] args) {
+//		
+//		menuList (5개)
+////		priceList (5개)  
+//		price
+//		
+//		price[status].
+//				
+//				{10, 100, 200, 300, 400}
+//		{객체, 객체, 객체}
+//				
+//				
+//		for(장바구니 모든 데이터 -> 5) {
+//			
+//			switch(menuList.get(i).getMenu   내가 주문한 메뉴의 종류) {
+//				case 샌드위치
+//								검증(menuList.get(i).getBread, menuList.get(i).);
+//								db에 가따와 테이블 5개를 조인해서 priceVO
+//								그 vo 객체를 리스트에 넣어 
+//								빵 테이블, 치즈 테이블, 미트 추가 테이블, 토핑 테이블, 세트 테이블
+//				case 샐러드
+//								검증2();
+//				case 랩
+//								if문 한번 (세트여부 )
+//				
+//			}
+//		}
+//		return	
+//	}
+//	
+//	public int 검증(빵, 치즈, 미트, 토핑, 세트) {
+//		여기서 검증해
+//		
+//		리턴 시켜줘 뭐뭐 했는지/
+//	}
+//	
+//}
