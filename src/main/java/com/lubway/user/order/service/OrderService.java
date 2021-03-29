ed1b147a586fe66a83ff8797c485ae2d0f0eb657
jieun <orbital0m0@naver.com>
2021-03-29 10:45:19 +0900

    order-mapping 추가

--- src/main/java/com/lubway/user/order/service/OrderService.java
+++ src/main/java/com/lubway/user/order/service/OrderService.java
@@ -1,10 +1,15 @@
 package com.lubway.user.order.service;
 
+import java.util.List;
+
 import com.lubway.store.StoreInfoVO;
 import com.lubway.user.menu.ToppingAddVO;
+import com.lubway.user.order.OrderVO;
 
 public interface OrderService {
 	StoreInfoVO getStoreInfoByNo(String franchiseNo);
 
 	ToppingAddVO getToppingByName(String t);
+	
+	List<OrderVO> getOrderList(OrderVO vo);
 }
--- src/main/java/com/lubway/user/order/service/OrderServiceImpl.java
+++ src/main/java/com/lubway/user/order/service/OrderServiceImpl.java
@@ -1,5 +1,7 @@
 package com.lubway.user.order.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,6 +9,8 @@ import com.lubway.store.StoreInfoDAO;
 import com.lubway.store.StoreInfoVO;
 import com.lubway.user.menu.ToppingAddVO;
 import com.lubway.user.menu.UserOptionDAO;
+import com.lubway.user.order.OrderDAO;
+import com.lubway.user.order.OrderVO;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -17,6 +21,9 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired
 	UserOptionDAO userOptionDao;
 	
+	@Autowired
+	OrderDAO orderDao;
+	
 	@Override
 	public StoreInfoVO getStoreInfoByNo(String franchiseNo) {
 		int no = Integer.parseInt(franchiseNo);
@@ -26,6 +33,11 @@ public class OrderServiceImpl implements OrderService {
 	@Override
 	public ToppingAddVO getToppingByName(String t) {
 		return userOptionDao.getToppingByName(t);
+	}
+
+	@Override
+	public List<OrderVO> getOrderList(OrderVO vo) {
+		return orderDao.getOrderList(vo);
 	}
 
 }
