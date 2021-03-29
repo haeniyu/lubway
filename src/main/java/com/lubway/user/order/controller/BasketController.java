db9518a40aaddf523508a506920a14ba1d7c4727
SoulTreeHyun <wldydgus@naver.com>
2021-03-29 18:33:40 +0900

    장바구니

--- src/main/java/com/lubway/user/order/controller/BasketController.java
+++ src/main/java/com/lubway/user/order/controller/BasketController.java
@@ -83,20 +83,77 @@ public class BasketController {
 			String step01Text, String step02Text, String step03Text,
 			String eachCost, String quantity, String totalPrice,
 			String franchiseNo, String whatWay, String code, String menuName,
-			String toppingAdd, String meatAdd, String cheeseAdd, String setAdd) {
+			String toppingAdd, String meatAdd, String cheeseAdd, String setAdd,
+			String vegetable, String sauce, BasketVO basket, String menu_type, String fullAddr) {
 		
 		System.out.println("장바구니 페이지 요청");
+		UserVO vo = (UserVO)session.getAttribute("user");
 		
-		BasketVO basket = new BasketVO();
 		String[] step01 = step01Text.trim().split(",");
+		String[] vegetableList = vegetable.trim().split(",");
+		String[] step03 = step03Text.trim().split(",");
+		if(step03.length != 1) {
+			String setName = step03[1].trim() + "/" + step03[2].trim();
+			basket.setSet_name(setName);
+			System.out.println(setName);
+		}
 		
-		System.out.println("step01Text : " + step01Text);
-		System.out.println("사이즈 : " + step01[0]);
-		System.out.println("빵 : " + step01[1]);
-		System.out.println("치즈 : " + step01[2]);
-		System.out.println("빵 : " + step01[3]);
-		System.out.println("빵 : " + step01[0]);
+		System.out.println("step03Text : " + step03Text);
 		
+		switch (code.substring(0, 3)) {
+		case "SDW" : 
+			break;
+		case "WRP" : 
+			break;
+		case "SLD" : 
+			break;
+		case "SMW" : 
+			break;
+		case "DRK" : 
+			break;
+		}
+		
+		String v = "";
+		for(int i=0; i<vegetableList.length; i++) {
+			if(i < vegetableList.length-1) v += vegetableList[i].trim() + ", ";
+			else v += vegetableList[i].trim();
+		}
+		System.out.println("v : " + v);
+
+		System.out.println("전 : " + basket);
+		
+		basket.setId(vo.getId());
+		basket.setTel(vo.getTel());
+		basket.setName(vo.getName());
+		if(step01[0].trim().equals("15cm")) basket.setSize(false);
+		else basket.setSize(true);
+		basket.setBread(step01[1].trim());
+		basket.setCheese(step01[2].trim());
+		basket.setVegetable(v);
+		basket.setSauce(sauce);
+		basket.setMeat(meatAdd);
+		basket.setTopping(toppingAdd);
+		basket.setSingle_price(eachCost);
+		if(!setAdd.equals("")) {
+			basket.setSet(true);
+			basket.setSet_price(setAdd);
+		} else {
+			basket.setSet(false);
+			basket.setSet_price(null);
+		}
+		basket.setTotal_price(totalPrice);
+		basket.setAdd_cheese(cheeseAdd);
+		basket.setMenu_name(menuName);
+//		basket.setMenu_filepath();
+		basket.setMenu_type(menu_type);
+		basket.setOrder_type(whatWay);
+		basket.setStore_no(Integer.parseInt(franchiseNo));
+		basket.setUser_address(fullAddr);
+		basket.setSet_name(null);
+			
+		System.out.println("후 : " + basket);
+		
+		/*
 		System.out.println("step02Text : " + step02Text);
 		System.out.println("step03Text : " + step03Text);
 		System.out.println("eachCost : " + eachCost);
@@ -110,6 +167,7 @@ public class BasketController {
 		System.out.println("meatAdd : " + meatAdd);
 		System.out.println("cheeseAdd : " + cheeseAdd);
 		System.out.println("setAdd : " + setAdd);
+		*/
 		
 		return "redirect:basketfast.do";
 	}
