98d85707d7004420abecca7773796502e87bf4aa
breezyyy <lee.jisoo0419@gmail.com>
2021-04-02 12:59:56 +0900

    포인트, 쿠폰 결제 연산 예외 처리 완료

--- src/main/webapp/resources/js/orderStep04.js
+++ src/main/webapp/resources/js/orderStep04.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 		if(percentageTmp.includes("사용가능")) percentageTmp="0";
 		var percentage = parseInt(percentageTmp);
 		
-		discount = (percentage/100)*savePrice;	//할인 금액
+		discount = parseInt((percentage/100)*savePrice);	//할인 금액
 		
 		price = savePrice - discount - pointAmt;
 		if(homewayDeliveryFee== 'Home-Way') price+=3900;
@@ -42,13 +42,27 @@ $(document).ready(function() {
 	
 	//포인트 모두 사용 눌렀을 경우
 	$("#useAllPoint").on("click", function(){
-		$("#pointAmt").val(USABLE_TMP);
-		$("#pointAmtNavi").text(USABLE_TMP);
-		$("#usablePoint").text("0");
+		
+		//잔여 결제금액 보다 보유 포인트 금액이 많을 경우
+		if(USABLE_POINT > price) {
+			$("#pointAmt").val(price.toString());
+			$("#pointAmtNavi").text(price.toString());
+			var point = USABLE_POINT - price;
+			$("#usablePoint").text(point.toString());
+			
+			pointAmt = price;
+			
+		}else{
+			$("#pointAmt").val(USABLE_TMP);
+			$("#pointAmtNavi").text(USABLE_TMP);
+			$("#usablePoint").text("0");
+			
+			pointAmt = USABLE_POINT;
+			
+		}
+
 		
 		//최종 결제 금액 - 포인트 사용 금액
-		pointAmt = USABLE_POINT;
-		
 		price = savePrice - discount - pointAmt;
 		if(homewayDeliveryFee== 'Home-Way') price+=3900;
 		
@@ -68,14 +82,26 @@ $(document).ready(function() {
 			if(homewayDeliveryFee== 'Home-Way') price+=3900;
 		}
 		
+		//입력한 포인트 금액에 대한 예외 처리
 		if(pointAmt>USABLE_POINT || pointAmt>price || pointAmt>savePrice){
 			alert("사용 가능한 포인트를 초과하였습니다.");
-			$("#pointAmt").val(USABLE_TMP);
-			$("#pointAmtNavi").text(USABLE_TMP);
-			$("#usablePoint").text("0");
-			$("#pointAmt").focus();
-			return;
+			
+			if(USABLE_POINT > price){
+				$("#pointAmt").val(price.toString());
+				$("#pointAmtNavi").text(price.toString());
+				var point = USABLE_POINT - price;
+				$("#usablePoint").text(point.toString());
+				$("#pointAmt").focus();
+				return;
+			} else {
+				$("#pointAmt").val(USABLE_TMP);
+				$("#pointAmtNavi").text(USABLE_TMP);
+				$("#usablePoint").text("0");
+				$("#pointAmt").focus();
+				return;
+			}
 		}
+		
 		$("#pointAmtNavi").text(pointAmt); 		//최종 결제 시 얼마가 쓰일지 표시해 줄 포인트
 		
 		//최종 결제 금액 - 포인트 사용 금액
