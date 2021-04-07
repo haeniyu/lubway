17e166592a74e86e7888f01ac54a3383801d5361
YunHyuna <hyun_a701@naver.com>
2021-04-07 18:30:19 +0900

    Revert "쿠폰 사용처리 수정"
    This reverts commit 55f727da204174b76fa336aec75774034ca0d999.

--- src/main/java/com/lubway/user/order/controller/OrderController.java
+++ src/main/java/com/lubway/user/order/controller/OrderController.java
@@ -338,7 +338,7 @@ public class OrderController {
 		userService.updateUser(user);
 		
 		//쿠폰 사용 처리
-		if(!couponCode.trim().equals("")) couponService.insertUseCoupon(couponCode, user.getId());
+		couponService.insertUseCoupon(couponCode, user.getId());
 		
 		return "redirect:/orderList.do";
 		
@@ -505,7 +505,7 @@ public class OrderController {
 		userService.updateUser(user);
 		
 		//쿠폰 사용 처리
-		if(!couponCode.trim().equals("")) couponService.insertUseCoupon(couponCode, user.getId());
+		couponService.insertUseCoupon(couponCode, user.getId());
 		
 		return "redirect:/orderList.do";
 		
