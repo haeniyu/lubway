a42d94bdd294e234c070c602eb29ac782b65018b
breezyyy <lee.jisoo0419@gmail.com>
2021-04-08 15:19:23 +0900

    Revert "PDF 다운로드 완료"
    This reverts commit 7b2a9a995dd82f1e3f968fa2846e6ba12722c0db.

--- src/main/java/com/lubway/admin/statistics/pdf/pdfController.java
+++ src/main/java/com/lubway/admin/statistics/pdf/pdfController.java
@@ -1,10 +1,10 @@
 package com.lubway.admin.statistics.pdf;
 
+import java.io.FileOutputStream;
 import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,49 +28,39 @@ import com.lubway.admin.statistics.service.StatService;
 
 @Controller
 public class pdfController {
-	
 	@Autowired
 	private StatService statservice;
 	
 	// PDF 생성
-	private Document makePdf(StatPagination sp, String fileName, 
-			HttpServletRequest request, HttpServletResponse response) throws Exception {
+	private Document makePdf(StatPagination sp) throws Exception {
 		
-			Document document = null;
-			
+		//검색 조건 설정
+		if(sp.getStore_name().equals("매장명 선택")) sp.setStore_name("");
+		if(sp.getOrder_type().equals("수령 방식 선택")) sp.setOrder_type("");
+		if(sp.getPayment_list().equals("결제 방식 선택")) sp.setPayment_list("");
+		if(sp.getStart().equals("")) sp.setStart("1000-01-01 00:00:00");
+		else sp.setStart(sp.getStart() + " 00:00:00");
+		if(sp.getEnd().equals("")) sp.setEnd("3000-12-31 23:59:59");
+		else sp.setEnd(sp.getEnd() + " 23:59:59");
+		
+		//검색 조건에 맞는 통계 결과를 가져옵니다.
+		List<StatVO> list = statservice.getSearchOrderListForExcel(sp);
+		//날짜 형식 변환
+		for(StatVO v : list) v.setRequest(new SimpleDateFormat("yyyy-MM-dd").format(v.getOrder_time()));
+		
+		//PDF 생성
+		Document document = new Document();
+		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("d:/sample.pdf"));
+		document.open();
 		try {
-			//검색 조건 설정
-			if(sp.getStore_name().equals("매장명 선택")) sp.setStore_name("");
-			if(sp.getOrder_type().equals("수령 방식 선택")) sp.setOrder_type("");
-			if(sp.getPayment_list().equals("결제 방식 선택")) sp.setPayment_list("");
-			if(sp.getStart().equals("")) sp.setStart("1000-01-01 00:00:00");
-			else sp.setStart(sp.getStart() + " 00:00:00");
-			if(sp.getEnd().equals("")) sp.setEnd("3000-12-31 23:59:59");
-			else sp.setEnd(sp.getEnd() + " 23:59:59");
-			
-			//검색 조건에 맞는 통계 결과를 가져옵니다.
-			List<StatVO> list = statservice.getSearchOrderListForExcel(sp);
-			//날짜 형식 변환
-			for(StatVO v : list) v.setRequest(new SimpleDateFormat("yyyy-MM-dd").format(v.getOrder_time()));
-			
-			//PDF 생성
-			document = new Document();
-			
-	        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8") + ".pdf");
-	        
-			PdfWriter writer = PdfWriter.getInstance(document, response.getOutputStream());
-	 		writer.setInitialLeading(12.5f);
-			document.setMargins(10, 10, 20, 20);
-			document.open();
 			
 			//폰트 설정
-			BaseFont baseFont = BaseFont.createFont(request.getServletContext().getRealPath("resources/font/NanumGothic.ttf"),
-					BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-			Font font = new Font(baseFont,10);
+			BaseFont baseFont = BaseFont.createFont("/webapp/resources/font/NanumGothic.ttf",BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+			Font font = new Font(baseFont,12);
 			
-			//테이블 생성
-			PdfPTable table = new PdfPTable(8);
-			table.setWidths(new int[]{3, 2, 2, 2, 2, 2, 2, 2});
+			//테이블 생성 (column,row)
+			PdfPTable table = new PdfPTable(7);
+			table.setPaddingTop(5);
 			
 			//타이틀 설정
 			Chunk chunk = new Chunk("러브웨이 매장별 매출 현황", font); // 타이틀 객체를 생성 (타이틀의 이름을 장바구니로 하고 위에 있는 font를 사용)
@@ -84,7 +74,6 @@ public class pdfController {
             
             PdfPCell cell1 = new PdfPCell(new Phrase("주문 날짜", font)); // 셀의 이름과 폰트를 지정해서 셀을 생성한다.
             cell1.setHorizontalAlignment(Element.ALIGN_CENTER); // 셀의 정렬방식을 지정한다. (가운데정렬)
-            cell1.setPadding(3);
 
             PdfPCell cell2 = new PdfPCell(new Phrase("매장명", font));
             cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
@@ -108,15 +97,10 @@ public class pdfController {
             cell8.setHorizontalAlignment(Element.ALIGN_CENTER);
  
             //테이블에 생성된 셀 삽입
-            table.addCell(cell1);
-            table.addCell(cell2);
-            table.addCell(cell3);
-            table.addCell(cell4);
-            table.addCell(cell5);
-            table.addCell(cell6);
-            table.addCell(cell7);
-            table.addCell(cell8);
-
+            for(int i=1; i<=8; i++) {
+            	String cell = "cell" + String.valueOf(i); 
+            	table.addCell(cell);
+            }
             
            //검색 결과 데이터를 삽입
             for (int i=0; i< list.size(); i++) {
@@ -129,7 +113,7 @@ public class pdfController {
             	PdfPCell cellTotal = new PdfPCell(new Phrase(String.valueOf(vo.getTotal()), font));
             	PdfPCell cellRealTotal = new PdfPCell(new Phrase(String.valueOf(vo.getReal_total()), font));
             	PdfPCell cellCoupon = new PdfPCell(new Phrase(String.valueOf(vo.getCoupon()), font));
-            	PdfPCell cellPoint = new PdfPCell(new Phrase(String.valueOf(vo.getPoint()), font));	
+            	PdfPCell cellPoint = new PdfPCell(new Phrase(vo.getPoint()));	//should be double-checked!!!!!!
             	
             	table.addCell(cellOrderDate);
             	table.addCell(cellStoreName);
@@ -142,7 +126,7 @@ public class pdfController {
             	
             }
 			document.add(table);
-			document.close();
+			
 		}catch (Exception e) {
 			System.out.println("만드는거부터 실패!!!~!~!~!" +  e);
 		}
@@ -150,13 +134,13 @@ public class pdfController {
 	}
 	
 	
-	//위에서 만들어진 PDF를 다운로드하는 함수
+	//위에서 만들어진 엑셀을 다운로드하는 함수
 	@RequestMapping(value="/pdfDownload.mdo")
-    public void pdfDownload(@RequestParam("start")String start, @RequestParam("end")String end, 
-    		@RequestParam("order_type")String order_type, @RequestParam("payment_list")String payment_list,
+    public void excelDownload(@RequestParam("start")String start, @RequestParam("end")String end, 
+    		@RequestParam("order_type")String order_type,
+    		@RequestParam("payment_list")String payment_list,
     		@RequestParam("store_name")String store_name,
-    		HttpServletRequest request, HttpServletResponse response,
-    		StatPagination sp) throws Exception {
+    		HttpServletResponse response, StatPagination sp) throws Exception {
        
 		try {
 			//검색 조건 설정
@@ -166,13 +150,19 @@ public class pdfController {
 			sp.setOrder_type(order_type);
 			sp.setPayment_list(payment_list);
 			
-			//파일명 설정
-	        String fileName = "매장별_매출현황";
-	        if(!start.trim().equals("") && !end.trim().equals("")) fileName = start + "_" + end + "_매출현황";
+			//액셀 생성
+            Document document = makePdf(sp);
+
+            //파일명 설정
+            String fileName = "매장별_매출현황";
+            if(!start.trim().equals("") && !end.trim().equals("")) fileName = start + "_" + end + "_매출현황";
+            System.out.println(fileName);
+            response.setContentType("ms-vnd/excel");
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8") + ".xlsx");
             
-            //PDF 생성
-            makePdf(sp, fileName, request, response);
-	        
+            //다운로드 실행
+            //document.write(response.getOutputStream());
+            document.close();
         } catch (Exception e) {
         	System.out.println("실패!!! " + e);
         }
